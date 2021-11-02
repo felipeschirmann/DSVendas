31b4b3e05dfcd25f4b88642aa9855b5cde639284
Felipe Schirmann <felipeschirmann@gmail.com>
2021-11-01 23:39:39 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1>Helow World</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container" >
+        <h1 className="text-primary">Helow World</h1>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
