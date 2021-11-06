40088ee0ebf85d7e187b97ffabc2a85ba06c8738
Felipe Schirmann <felipeschirmann@gmail.com>
2021-11-06 18:49:38 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { type } from "os";
+import { Seller } from "./seller";
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+};
+
+export type SalePage = {
+  content?: Sale[];
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  numberOfElements?: number;
+  first: boolean;
+  number: number;
+  size?: number;
+  empty?: boolean;
+};
+
 export type SaleSum = {
   sellerName: string;
   sum: number;
