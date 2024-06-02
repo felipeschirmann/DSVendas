ab2c7daa82b17f4c9903069e8ac031290375cef9
felipeschirmann <contato@felipeschirmann.dev.br>
2024-06-02 15:30:58 -0300

    Update backend to SpringBoot 3.3.x

--- backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.config;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,35 +10,56 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 	@Autowired
 	private Environment env;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-			http.headers().frameOptions().disable();
-		}
-		
-		http.cors().and().csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
-	}
-
 	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http
+			.authorizeHttpRequests((requests) -> requests
+					.requestMatchers(toH2Console()).permitAll() // <-
+			)
+			.csrf((protection) -> protection
+                    .ignoringRequestMatchers(toH2Console()) // <- 
+            )
+			.cors((cors) -> cors
+					.configurationSource(profileTesConfigurationSource())
+			);
+		}
+        http
+            .authorizeHttpRequests((requests) -> requests
+                // another matchers
+            	.requestMatchers("/**").permitAll()	
+                .anyRequest().authenticated()
+            )
+//            .formLogin((login) -> login
+//                .loginPage("/login")
+//                .permitAll()
+//            )
+//            .logout((logout) -> logout
+//                .permitAll()
+//            )
+            .headers((header) -> header
+                .frameOptions().sameOrigin()
+            );
+        return http.build();
+    }
+
+	CorsConfigurationSource profileTesConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList(env.getProperty("cors.allow.frontend")));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
